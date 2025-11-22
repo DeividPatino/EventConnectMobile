44c1451fa2541ae7ca87d59f52b914f9f78915e9
CaledVivanco <147743298+CaledVivanco@users.noreply.github.com>
2025-11-22 06:03:41 -0500

    Add lazy loading routes for organizer panel

--- src/app/pages/organizer-panel/organizer-panel-routing.module.ts
+++ src/app/pages/organizer-panel/organizer-panel-routing.module.ts
@@ -7,6 +7,22 @@ const routes: Routes = [
   {
     path: '',
     component: OrganizerPanelPage
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'mis-eventos',
+    loadChildren: () => import('./mis-eventos/mis-eventos.module').then( m => m.MisEventosPageModule)
+  },
+  {
+    path: 'nuevo-evento',
+    loadChildren: () => import('./nuevo-evento/nuevo-evento.module').then( m => m.NuevoEventoPageModule)
+  },
+  {
+    path: 'mi-perfil',
+    loadChildren: () => import('./mi-perfil/mi-perfil.module').then( m => m.MiPerfilPageModule)
   }
 ];
 
