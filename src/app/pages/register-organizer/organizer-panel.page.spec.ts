5f1ca32c3cab6601c48cbe3c577c433de644decc
CaledVivanco <147743298+CaledVivanco@users.noreply.github.com>
2025-11-22 06:06:18 -0500

    Delete OrganizerPanelPage test suite
    Removed tests for OrganizerPanelPage.

--- src/app/pages/register-organizer/organizer-panel.page.spec.ts
+++ src/app/pages/register-organizer/organizer-panel.page.spec.ts
@@ -1,17 +1,1 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { OrganizerPanelPage } from './organizer-panel.page';
 
-describe('OrganizerPanelPage', () => {
-  let component: OrganizerPanelPage;
-  let fixture: ComponentFixture<OrganizerPanelPage>;
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OrganizerPanelPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
